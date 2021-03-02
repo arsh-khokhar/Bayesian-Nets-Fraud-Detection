acadcfd8102fd2a7cdb796c7dcb2649cc6346bf9
KiernanW <kiernanw@shaw.ca>
2021-03-01 19:14:02 -0600

    Create main.py, add todo list to readme.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Bayesian-Nets-Fraud-Detection
 Bayesian Network based Fraud Detection system
+
+## Things to do:
+-Figure out where the heck to actually start
+-Draw a graph representing the network (and the tables?)
+-????
---
+++ main.py
@@ -0,0 +1,48 @@
+class Factor:
+    def __init__(size):
+        print('2-D list + other stuff?')
+
+
+def observe(factor, variable, value):
+    """
+    Restrict a variable to some value in a given factor
+    :param factor:
+    :param variable:
+    :param value:
+    :return:
+    """
+    print('hello world')
+
+
+def multiply(factor1, factor2):
+    """
+    Multiply two factors
+    :param factor1:
+    :param factor2:
+    :return:
+    """
+    print('hello world')
+
+
+def normalize(factor):
+    """
+    Normalize a factor by dividing each entry by the sum of all the entries.
+    This is useful when the factor is a distribution (i.e. sum of the
+    probabilities must be 1).
+    :param factor:
+    :return:
+    """
+    print('hello world')
+
+
+def inference(factor_list, query_variables, ordered_list_of_hidden_variables,
+              evidence_list):
+    """
+    Too long to write here
+    :param factor_list:
+    :param query_variables:
+    :param ordered_list_of_hidden_variables:
+    :param evidence_list:
+    :return:
+    """
+    print('hello world')
